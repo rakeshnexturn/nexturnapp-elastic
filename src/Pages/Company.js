70f5fce93be5a4fc10fe2dd9b0e26671e5f7c89e
Rakesh Kumar Sahu <118262896+rakeshnexturn@users.noreply.github.com>
2023-09-29 09:38:57 +0530

    Update Company.js

--- src/Pages/Company.js
+++ src/Pages/Company.js
@@ -1,17 +1,34 @@
-// Home.js
-import React from "react";
+import React, { useEffect, useState } from "react";
 import mobilesData from "./mobiles.json";
 
 const CompanyInfo = () => {
+  const [awsRegion, setAwsRegion] = useState("Unknown AWS Region");
+
+  useEffect(() => {
+    // Fetch AWS region information (replace this with your actual logic)
+    const fetchAwsRegion = async () => {
+      try {
+        // Example: Fetching the AWS region from a data source or API
+        const response = await fetch("api/aws/region");
+        const data = await response.json();
+        setAwsRegion(data.region);
+      } catch (error) {
+        console.error("Error fetching AWS region:", error);
+      }
+    };
+
+    fetchAwsRegion();
+  }, []);
+
   return (
     <div className="container">
       <div className="company-info">
         <p>
           Welcome to NEXTURN Company! We are a leading provider of innovative
-          solutions in various industries. With our dedication to excellence and
-          customer satisfaction, we aim to deliver the best services to our
-          clients. Our team of experts is committed to pushing boundaries and
-          achieving remarkable results.
+          solutions in AWS region: {awsRegion}. With our dedication to
+          excellence and customer satisfaction, we aim to deliver the best
+          services to our clients. Our team of experts is committed to pushing
+          boundaries and achieving remarkable results.
         </p>
       </div>
     </div>
@@ -19,3 +36,4 @@ const CompanyInfo = () => {
 };
 
 export default CompanyInfo;
+
